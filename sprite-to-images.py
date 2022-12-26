42bed1245fe81e461d8751d85ed7154ee21cc995
Ongezell <65968666+ongezell@users.noreply.github.com>
2022-12-26 20:03:32 -0300

    Update sprite-to-images.py

--- sprite-to-images.py
+++ sprite-to-images.py
@@ -3,28 +3,19 @@
 import os
 from PIL import Image
 
-# Set the path to the sprite sheet file
 sprite_sheet_path = "sprite-sheet.png"
-
-# Load the sprite sheet
 sprite_sheet = Image.open(sprite_sheet_path)
 
-# Get the width and height of the sprite sheet
 sprite_sheet_width, sprite_sheet_height = sprite_sheet.size
-
-# Prompt the user to enter the output width and height of the images
 output_width = int(input("Enter the output width of the images: "))
 output_height = int(input("Enter the output height of the images: "))
-
-# Calculate the number of images that will be generated from the sprite sheet
 num_images = sprite_sheet_width // output_width
 
-# Create a new folder to hold the images
+
 output_folder_path = "output"
 if not os.path.exists(output_folder_path):
   os.makedirs(output_folder_path)
 
-# Split the sprite sheet into individual images
 for i in range(num_images):
   x1 = i * output_width
   y1 = 0
